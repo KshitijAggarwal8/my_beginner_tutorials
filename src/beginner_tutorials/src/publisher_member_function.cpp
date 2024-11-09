a788413c149451b235b8e168589c54cd98fbb59c
KshitijAggarwal8 <kshitijaggarwal@gmail.com>
2024-11-08 21:58:01 -0500

    Formatted for Google styles. Added Doxygen comments.

--- src/beginner_tutorials/src/publisher_member_function.cpp
+++ src/beginner_tutorials/src/publisher_member_function.cpp
@@ -1,3 +1,31 @@
+/**
+* @file publisher_member_function.cpp
+* @author Kshitij Aggarwal - 119211618
+* @brief
+* @version 0.1
+* @date 2024-11-08
+*
+* @copyright Copyright (c) 2024 Kshitij Aggarwal
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*
+*/
+
 #include <chrono>   // NOLINT(build/c++11)
 #include <functional>
 #include <memory>
@@ -10,14 +38,14 @@
 
 using std::chrono_literals::operator""ms;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
- * member function as a callback from the timer. */
-
+/**
+* @brief Publisher Node that publishes to /topic, with parameterized publishing
+* frequency and echoes it to RCLCPP_INFO logging level.
+*/
 
 class MinimalPublisher : public rclcpp::Node {
  public:
   MinimalPublisher() : Node("minimal_publisher"), count_(0) {
-
     // Declare the parameter
     this->declare_parameter("publish_frequency", 1.0);  // Default value 1 Hz
 
@@ -26,24 +54,30 @@ class MinimalPublisher : public rclcpp::Node {
     this->get_parameter("publish_frequency", publish_frequency);
 
     // Convert frequency to milliseconds
-    auto timer_period = std::chrono::milliseconds(static_cast<int>(1000 / publish_frequency));
+    auto timer_period = std::chrono::milliseconds
+                                (static_cast<int>(1000 / publish_frequency));
 
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
 
     timer_ = this->create_wall_timer(
         timer_period, std::bind(&MinimalPublisher::timer_callback, this));
 
-    if(count_ > 10000)
-      RCLCPP_FATAL(this->get_logger(), "Node running for too long asynchronously!");
+    if (count_ > 10000)
+      RCLCPP_FATAL(this->get_logger(),
+                      "Node running for too long asynchronously!");
 
 
-    // Service
+ /**
+* @brief Service to dynamically update the string message being published
+*
+*/
+
     service_ = this->create_service<beginner_tutorials::srv::SetString>(
         "set_message",
-        std::bind(&MinimalPublisher::handle_set_message, this, std::placeholders::_1, std::placeholders::_2));
-    RCLCPP_DEBUG_STREAM(this->get_logger(), "Service 'set_string' is ready to accept requests." << 4);
-    // RCLCPP_INFO(this->get_logger(), "Service 'set_string' is ready to accept requests.");
-
+        std::bind(&MinimalPublisher::handle_set_message,
+        this, std::placeholders::_1, std::placeholders::_2));
+    RCLCPP_DEBUG_STREAM(this->get_logger(),
+    "Service 'set_string' is ready to accept requests." << 4);
   }
   std::string current_message_ = "Hello ENPM700! 0";  // Default message
 
@@ -51,26 +85,29 @@ class MinimalPublisher : public rclcpp::Node {
   void timer_callback() {
     auto message = std_msgs::msg::String();
 
-    // message.data = "Hello ENPM700! " + std::to_string(count_++);
     message.data = current_message_ + std::to_string(count_++);
 
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
 
+/**
+* @brief Updating the message with the request received via service
+*/
   void handle_set_message(
-      const std::shared_ptr<beginner_tutorials::srv::SetString::Request> request,
+      const std::shared_ptr<beginner_tutorials::srv::SetString::Request>
+      request,
       std::shared_ptr<beginner_tutorials::srv::SetString::Response> response) {
     std::string new_message = request->data;
-    // current_message_ = request->data;
-    if(new_message == ""){
+
+    if (new_message == "") {
       RCLCPP_ERROR(this->get_logger(), "EMPTY MESSAGE. Cannot update!");
-    }
-    else{
+    } else {
       current_message_ = new_message;
       response->success = true;
       response->message = "Output string changed to: " + current_message_;
-      RCLCPP_WARN(this->get_logger(), "Output string updated to: '%s'", current_message_.c_str());
+      RCLCPP_WARN(this->get_logger(), "Output string updated to: '%s'",
+      current_message_.c_str());
     }
   }
 
@@ -78,7 +115,6 @@ class MinimalPublisher : public rclcpp::Node {
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   rclcpp::Service<beginner_tutorials::srv::SetString>::SharedPtr service_;
   size_t count_;
-  
 };
 
 int main(int argc, char* argv[]) {
--- src/beginner_tutorials/src/subscriber_member_function.cpp
+++ src/beginner_tutorials/src/subscriber_member_function.cpp
@@ -5,7 +5,7 @@
  * @version 0.1
  * @date 2024-11-08
  *
- * @copyright Copyright (c) 2023 Kshitij Aggarwal
+ * @copyright Copyright (c) 2024 Kshitij Aggarwal
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
   in the Software without restriction, including without limitation the rights
