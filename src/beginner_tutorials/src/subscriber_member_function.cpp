2b3845f850cb4e4c33dedb39abef10210881b176
KshitijAggarwal8 <kshitijaggarwal@gmail.com>
2024-11-08 20:42:31 -0500

    Added service to update published string. Updated logging information messages 5 types.

---
+++ src/beginner_tutorials/src/subscriber_member_function.cpp
@@ -0,0 +1,69 @@
+/**
+ * @file subscriber_member_function.cpp
+ * @author Kshitij Aggarwal - 119211618
+ * @brief
+ * @version 0.1
+ * @date 2024-11-08
+ *
+ * @copyright Copyright (c) 2023 Kshitij Aggarwal
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+ *
+ */
+#include <memory>
+#include "rclcpp/rclcpp.hpp"
+#include <std_msgs/msg/string.hpp>
+
+/**
+ * @brief Minimal Subscriber Node that listens to /topic and echoes it to
+ * RCLCPP_INFO logging level
+ *
+ */
+class MinimalSubscriber : public rclcpp::Node {
+ public:
+  /**
+   * @brief Construct a new Minimal Subscriber object and instantiate the
+   * subscriber object for the /topic with the topic_callback
+   *
+   */
+  MinimalSubscriber() : Node("minimal_subscriber") {
+    subscription_ = this->create_subscription<std_msgs::msg::String>(
+        "topic", 10,
+        std::bind(&MinimalSubscriber::topic_callback, this,
+                  std::placeholders::_1));
+  }
+
+ private:
+  /**
+   * @brief /topic subscriber callback that echoes the message to RCLCPP_INFO
+   * logging level
+   *
+   * @param msg
+   */
+  void topic_callback(const std_msgs::msg::String::SharedPtr msg) const {
+    RCLCPP_INFO_STREAM(this->get_logger(), "I heard: '" << msg->data.c_str());
+  }
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+
+int main(int argc, char* argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
